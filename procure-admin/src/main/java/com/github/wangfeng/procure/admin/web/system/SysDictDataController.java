441b059b4ae769a6f55426025c1bf27e034906d7
wangfeng <wangfeng@unionpayintl.com>
2019-10-30 15:03:00 +0800

    多层字典数据

--- procure-admin/src/main/java/com/github/wangfeng/procure/admin/web/system/SysDictDataController.java
+++ procure-admin/src/main/java/com/github/wangfeng/procure/admin/web/system/SysDictDataController.java
@@ -20,17 +20,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 数据字典信息
- * 
+ *
  * @author wangfeng
  */
 @Controller
 @RequestMapping("/system/dict/data")
-public class SysDictDataController extends BaseController
-{
+public class SysDictDataController extends BaseController {
+
     private String prefix = "system/dict/data";
 
     @Autowired
@@ -38,27 +39,42 @@ public class SysDictDataController extends BaseController
 
     @RequiresPermissions("system:dict:view")
     @GetMapping()
-    public String dictData()
-    {
+    public String dictData() {
         return prefix + "/data";
     }
+
+    //@PostMapping("/list")
+    //@RequiresPermissions("system:dict:list")
+    //@ResponseBody
+    //public TableDataInfo list(SysDictDataDO dictData)
+    //{
+    //    startPage();
+    //    List<SysDictDataDO> list = dictDataService.selectDictDataList(dictData);
+    //    return getDataTable(list);
+    //}
 
     @PostMapping("/list")
     @RequiresPermissions("system:dict:list")
     @ResponseBody
-    public TableDataInfo list(SysDictDataDO dictData)
-    {
-        startPage();
+    public List<SysDictDataDO> list(SysDictDataDO dictData, @RequestParam("dictType") String dictType) {
+        //startPage();
+        if (dictData == null) {
+            dictData = new SysDictDataDO();
+        }
+
+        if (dictType != null) {
+            dictData.setDictType(dictType);
+        }
+
         List<SysDictDataDO> list = dictDataService.selectDictDataList(dictData);
-        return getDataTable(list);
+        return list;
     }
 
     @Log(title = "字典数据", businessType = BusinessType.EXPORT)
     @RequiresPermissions("system:dict:export")
     @PostMapping("/export")
     @ResponseBody
-    public AjaxResult export(SysDictDataDO dictData)
-    {
+    public AjaxResult export(SysDictDataDO dictData) {
         List<SysDictDataDO> list = dictDataService.selectDictDataList(dictData);
         ExcelUtil<SysDictDataDO> util = new ExcelUtil<SysDictDataDO>(SysDictDataDO.class);
         return util.exportExcel(list, "字典数据");
@@ -68,8 +84,17 @@ public class SysDictDataController extends BaseController
      * 新增字典类型
      */
     @GetMapping("/add/{dictType}")
-    public String add(@PathVariable("dictType") String dictType, ModelMap mmap)
-    {
+    public String add(@PathVariable("dictType") String dictType, ModelMap mmap) {
+        mmap.put("parentCode", 0);
+        if (dictType.matches("^\\d*$")) {
+            //若是数组，说明是添加子项
+            Long dictCode = Long.parseLong(dictType);
+            mmap.put("parentCode", dictCode);
+            SysDictDataDO sysDictDataDO = dictDataService.selectDictDataById(dictCode);
+            if (sysDictDataDO != null) {
+                dictType = sysDictDataDO.getDictType();
+            }
+        }
         mmap.put("dictType", dictType);
         return prefix + "/add";
     }
@@ -81,8 +106,7 @@ public class SysDictDataController extends BaseController
     @RequiresPermissions("system:dict:add")
     @PostMapping("/add")
     @ResponseBody
-    public AjaxResult addSave(@Validated SysDictDataDO dict)
-    {
+    public AjaxResult addSave(@Validated SysDictDataDO dict) {
         dict.setCreateBy(ShiroUtils.getLoginName());
         return toAjax(dictDataService.insertDictData(dict));
     }
@@ -91,8 +115,7 @@ public class SysDictDataController extends BaseController
      * 修改字典类型
      */
     @GetMapping("/edit/{dictCode}")
-    public String edit(@PathVariable("dictCode") Long dictCode, ModelMap mmap)
-    {
+    public String edit(@PathVariable("dictCode") Long dictCode, ModelMap mmap) {
         mmap.put("dict", dictDataService.selectDictDataById(dictCode));
         return prefix + "/edit";
     }
@@ -104,8 +127,7 @@ public class SysDictDataController extends BaseController
     @RequiresPermissions("system:dict:edit")
     @PostMapping("/edit")
     @ResponseBody
-    public AjaxResult editSave(@Validated SysDictDataDO dict)
-    {
+    public AjaxResult editSave(@Validated SysDictDataDO dict) {
         dict.setUpdateBy(ShiroUtils.getLoginName());
         return toAjax(dictDataService.updateDictData(dict));
     }
@@ -114,8 +136,7 @@ public class SysDictDataController extends BaseController
     @RequiresPermissions("system:dict:remove")
     @PostMapping("/remove")
     @ResponseBody
-    public AjaxResult remove(String ids)
-    {
+    public AjaxResult remove(String ids) {
         return toAjax(dictDataService.deleteDictDataByIds(ids));
     }
 }
